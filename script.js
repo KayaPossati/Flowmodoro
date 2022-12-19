8edcbe207ce35855251917334de58c40bf308021
Kaya Possati <70762757+KayaPossati@users.noreply.github.com>
2022-12-19 14:03:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var running = false
 var interval
 var minutes = 0
 var seconds = 0
+var sidehours = 0
+var sideminutes = ''
+var sideseconds = 0
 
 function start() {
   if (!running) {
@@ -10,29 +13,71 @@ function start() {
   }
 }
 
+function switchPomodoro() {
+  var pomoside = document.getElementById('pomoside')
+  var flowside = document.getElementById('flowside')
+
+  pomoside.classList.add('side-atv')
+  flowside.classList.remove('side-atv')
+}
+
+function switchFlow() {
+  var pomoside = document.getElementById('pomoside')
+  var flowside = document.getElementById('flowside')
+
+  flowside.classList.add('side-atv')
+  pomoside.classList.remove('side-atv')
+}
+
 function startTimer() {
   running = true
   var mode = document.getElementById('mode')
   var timer = document.getElementById('time')
+  var bg = document.getElementById('background')
+  var sidetimer = document.getElementById('sidetime')
+
   if (mode.innerHTML == 'Work Time') {
     minutes = ''
     seconds = 0
     interval = setInterval(() => {
       seconds++
+      sideseconds++
+
       if (seconds >= 60) {
         minutes++
         seconds = 0
       }
+      if (sideseconds >= 60) {
+        sideminutes++
+        sideseconds = 0
+      }
+      if (sideminutes >= 60) {
+        sidehours++
+        sideminutes = 0
+      }
+
       timer.innerHTML =
         (minutes < 1 ? '00' : minutes < 10 ? '0' : '') +
         minutes +
         ':' +
         (seconds < 10 ? '0' : '') +
         seconds
+      sidetimer.innerHTML =
+        (sidehours == 0 ? '' : sidehours + ':') +
+        (sideminutes < 1 ? '00' : sideminutes < 10 ? '0'+sideminutes : sideminutes) +
+        ':' +
+        (sideseconds < 10 ? '0' : '') +
+        sideseconds
     }, 1000)
   }
 
-  if (mode.innerHTML == 'Break Time') {
+  if (mode.innerHTML == 'Break Time' && timer.innerHTML == '00:00') {
+    mode.innerHTML = 'Work Time'
+    bg.classList.add('work')
+    bg.classList.remove('break')
+  }
+
+  if (mode.innerHTML == 'Break Time' && timer.innerHTML != '00:00') {
     var countDown = seconds + minutes * 60
     minutes = Math.round(countDown / 60)
     seconds = Math.round(countDown % 60)
@@ -82,5 +127,3 @@ function stopTimer() {
     bg.classList.remove('break')
   }
 }
-
-
